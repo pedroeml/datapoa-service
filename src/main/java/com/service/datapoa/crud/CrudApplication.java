bc1605268b7c403e16a1d8697bcf95ee2190a1e9
Pedro Lemos <pedroemlemos@gmail.com>
2020-01-12 00:03:23 -0300

    CrudApplication updates

--- src/main/java/com/service/datapoa/crud/CrudApplication.java
+++ src/main/java/com/service/datapoa/crud/CrudApplication.java
@@ -2,16 +2,14 @@ package com.service.datapoa.crud;
 
 import com.service.datapoa.crud.linhaonibus.model.LinhaOnibusModel;
 import com.service.datapoa.crud.linhaonibus.rest.LinhaOnibusService;
-import com.service.datapoa.crud.pontotaxi.jpa.PontoTaxi;
-import com.service.datapoa.crud.pontotaxi.jpa.PontoTaxiRepository;
+import com.service.datapoa.crud.pontotaxi.model.PontoTaxiModel;
+import com.service.datapoa.crud.pontotaxi.rest.PontoTaxiService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -24,31 +22,13 @@ public class CrudApplication {
         SpringApplication.run(CrudApplication.class, args);
     }
 
-    @Autowired
-    private LinhaOnibusService linhasOnibusService;
-
     @Bean
-    public CommandLineRunner demo(PontoTaxiRepository repository) {
+    public CommandLineRunner demo(PontoTaxiService pontoTaxiService) {
         return args -> {
-            repository.save(new PontoTaxi("PONTO-ZONA-SUL", "-30.12373379817800000", "-51.22266028234100000", "2019-02-10T16:14:34.828"));
-            repository.save(new PontoTaxi("PONTO-ZONA-NORTE-1", "-30.0103346", "-51.1724526", "2019-03-10T16:14:34.828"));
+            final List<PontoTaxiModel> pontos = pontoTaxiService.findAll();
 
-            log.info("PontoTaxi found with findAll()");
-            log.info("------------------------------");
-            for (PontoTaxi ponto : repository.findAll()) {
-                log.info(ponto.toString());
-            }
-            log.info("");
-
-            final PontoTaxi pontoTaxi = repository.findById(1L);
-            log.info("PontoTaxi found with findById(1L)");
-            log.info("---------------------------------");
-            log.info(pontoTaxi.toString());
-            log.info("");
-
-            log.info("PontoTaxi found with findByName('PONTO-ZONA-NORTE-1')");
-            log.info("-----------------------------------------------------");
-            final List<PontoTaxi> pontos = repository.findByName("PONTO-ZONA-NORTE-1");
+            log.info("List of PontoTaxi found with findAll()");
+            log.info("--------------------------------------");
             pontos.forEach(ponto -> {
                 log.info(ponto.toString());
             });
@@ -57,7 +37,7 @@ public class CrudApplication {
     }
 
     @Bean
-    public CommandLineRunner run(RestTemplate restTemplate) {
+    public CommandLineRunner run(LinhaOnibusService linhasOnibusService) {
         return args -> {
             final List<LinhaOnibusModel> linhas = linhasOnibusService.findAll();
 
