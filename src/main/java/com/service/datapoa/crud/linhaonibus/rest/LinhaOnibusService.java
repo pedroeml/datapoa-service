eb2cf9c9c6bfa7cd9e4812f17b6245a762677201
Pedro Lemos <pedroemlemos@gmail.com>
2020-01-13 13:40:57 -0300

    LinhaOnibusService implementing Crud interface

--- src/main/java/com/service/datapoa/crud/linhaonibus/rest/LinhaOnibusService.java
+++ src/main/java/com/service/datapoa/crud/linhaonibus/rest/LinhaOnibusService.java
@@ -1,5 +1,6 @@
 package com.service.datapoa.crud.linhaonibus.rest;
 
+import com.service.datapoa.crud.Crud;
 import com.service.datapoa.crud.linhaonibus.integration.LinhaOnibusResponse;
 import com.service.datapoa.crud.linhaonibus.mapper.LinhaOnibusMapper;
 import com.service.datapoa.crud.linhaonibus.model.LinhaOnibusModel;
@@ -15,18 +16,50 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class LinhaOnibusService {
+public class LinhaOnibusService implements Crud<LinhaOnibusModel> {
     private static final String url = "http://www.poatransporte.com.br/php/facades/process.php?a=nc&p=%&t=o";
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @Override
+    public LinhaOnibusModel findById(long id) {
+        return null;
+    }
+
+    @Override
     public List<LinhaOnibusModel> findAll() {
         final ResponseEntity<List<LinhaOnibusResponse>> response = this.restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<LinhaOnibusResponse>>() { });
         final List<LinhaOnibusResponse> linhasResponse = response.getBody();
 
+        // TODO: Add handler for finding objects with same ID in the MySQL DB
+
         return linhasResponse == null ? Collections.emptyList() : linhasResponse.stream()
             .map(LinhaOnibusMapper::mapToModel)
             .collect(Collectors.toList());
+    }
+
+    @Override
+    public LinhaOnibusModel add(LinhaOnibusModel linhaOnibusModel) {
+        // TODO: Implementation
+        return null;
+    }
+
+    @Override
+    public LinhaOnibusModel update(long id, LinhaOnibusModel linhaOnibusModel) {
+        // TODO: Implementation
+        return null;
+    }
+
+    @Override
+    public LinhaOnibusModel replace(long id, LinhaOnibusModel linhaOnibusModel) {
+        // TODO: Implementation
+        return null;
+    }
+
+    @Override
+    public LinhaOnibusModel delete(long id) {
+        // TODO: Implementation
+        return null;
     }
 }
