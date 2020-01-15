a4479a60f77d5087cb6833f0e6870238072419d0
Pedro Lemos <pedroemlemos@gmail.com>
2020-01-15 12:57:27 -0300

    LinhaOnibusService find by ID method implementation

--- src/main/java/com/service/datapoa/crud/linhaonibus/rest/LinhaOnibusService.java
+++ src/main/java/com/service/datapoa/crud/linhaonibus/rest/LinhaOnibusService.java
@@ -2,18 +2,22 @@ package com.service.datapoa.crud.linhaonibus.rest;
 
 import com.service.datapoa.crud.Crud;
 import com.service.datapoa.crud.linhaonibus.dao.LinhaOnibusDAO;
+import com.service.datapoa.crud.linhaonibus.dao.jpa.LinhaOnibus;
 import com.service.datapoa.crud.linhaonibus.integration.LinhaOnibusResponse;
 import com.service.datapoa.crud.linhaonibus.mapper.LinhaOnibusMapper;
 import com.service.datapoa.crud.linhaonibus.model.LinhaOnibusModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -28,21 +32,40 @@ public class LinhaOnibusService implements Crud<LinhaOnibusModel> {
 
     @Override
     public LinhaOnibusModel findById(long id) {
-        // TODO: Add handler for not finding an object with the specified ID
-        // TODO: Implementation
-        return null;
+        final LinhaOnibus linha = this.dao.get(id);
+        LinhaOnibusModel linhaOnibus;
+
+        if (linha == null) {
+            List<LinhaOnibusModel> linhas = this.findAllFromExternalAPI();
+            final Optional<LinhaOnibusModel> maybeLinha = linhas.parallelStream()
+                .filter(linhaOnibusModel -> linhaOnibusModel.getId() == id)
+                .findFirst();
+
+            if (maybeLinha.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("PontoTaxi ID %d not found", id));
+            }
+
+            linhaOnibus = maybeLinha.get();
+        } else {
+            linhaOnibus = LinhaOnibusMapper.mapToModel(linha);
+        }
+
+        return linhaOnibus;
+    }
+
+    private List<LinhaOnibusModel> findAllFromExternalAPI() {
+        final ResponseEntity<List<LinhaOnibusResponse>> response = this.restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<LinhaOnibusResponse>>() { });
+        final List<LinhaOnibusResponse> linhasResponse = response.getBody();
+
+        return linhasResponse == null ? Collections.emptyList() : linhasResponse.stream()
+                .map(LinhaOnibusMapper::mapFromResponse)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<LinhaOnibusModel> findAll() {
-        final ResponseEntity<List<LinhaOnibusResponse>> response = this.restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<LinhaOnibusResponse>>() { });
-        final List<LinhaOnibusResponse> linhasResponse = response.getBody();
-
-        // TODO: Add handler for finding objects with same ID in the MySQL DB
-
-        return linhasResponse == null ? Collections.emptyList() : linhasResponse.stream()
-            .map(LinhaOnibusMapper::mapFromResponse)
-            .collect(Collectors.toList());
+        // TODO: Add handler for finding and replacing objects with same ID in the MySQL DB
+        return this.findAllFromExternalAPI();
     }
 
     @Override
