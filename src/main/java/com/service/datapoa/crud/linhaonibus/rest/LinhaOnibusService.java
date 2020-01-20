e1475cb845a8927871e6c3a1d0e2df9b8fa4635d
Pedro Lemos <pedroemlemos@gmail.com>
2020-01-20 13:27:27 -0300

    LinhaOnibusService implementation

--- src/main/java/com/service/datapoa/crud/linhaonibus/rest/LinhaOnibusService.java
+++ src/main/java/com/service/datapoa/crud/linhaonibus/rest/LinhaOnibusService.java
@@ -3,93 +3,109 @@ package com.service.datapoa.crud.linhaonibus.rest;
 import com.service.datapoa.crud.Crud;
 import com.service.datapoa.crud.linhaonibus.dao.LinhaOnibusDAO;
 import com.service.datapoa.crud.linhaonibus.dao.jpa.LinhaOnibus;
-import com.service.datapoa.crud.linhaonibus.integration.LinhaOnibusResponse;
 import com.service.datapoa.crud.linhaonibus.mapper.LinhaOnibusMapper;
 import com.service.datapoa.crud.linhaonibus.model.LinhaOnibusModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class LinhaOnibusService implements Crud<LinhaOnibusModel> {
-    private static final String url = "http://www.poatransporte.com.br/php/facades/process.php?a=nc&p=%&t=o";
-
-    @Autowired
-    private RestTemplate restTemplate;
 
     @Autowired
     private LinhaOnibusDAO dao;
 
+    @Autowired
+    private LinhaOnibusRestService restService;
+
     @Override
     public LinhaOnibusModel findById(long id) {
         final LinhaOnibus linha = this.dao.get(id);
-        LinhaOnibusModel linhaOnibus;
-
-        if (linha == null) {
-            List<LinhaOnibusModel> linhas = this.findAllFromExternalAPI();
-            final Optional<LinhaOnibusModel> maybeLinha = linhas.parallelStream()
-                .filter(linhaOnibusModel -> linhaOnibusModel.getId() == id)
-                .findFirst();
-
-            if (maybeLinha.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("PontoTaxi ID %d not found", id));
-            }
-
-            linhaOnibus = maybeLinha.get();
-        } else {
-            linhaOnibus = LinhaOnibusMapper.mapToModel(linha);
-        }
-
-        return linhaOnibus;
-    }
-
-    private List<LinhaOnibusModel> findAllFromExternalAPI() {
-        final ResponseEntity<List<LinhaOnibusResponse>> response = this.restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<LinhaOnibusResponse>>() { });
-        final List<LinhaOnibusResponse> linhasResponse = response.getBody();
-
-        return linhasResponse == null ? Collections.emptyList() : linhasResponse.stream()
-                .map(LinhaOnibusMapper::mapFromResponse)
-                .collect(Collectors.toList());
+        return linha == null ? this.restService.findById(id) : LinhaOnibusMapper.mapToModel(linha);
     }
 
     @Override
     public List<LinhaOnibusModel> findAll() {
-        // TODO: Add handler for finding and replacing objects with same ID in the MySQL DB
-        return this.findAllFromExternalAPI();
+        List<LinhaOnibusModel> linhasFromDB = this.dao.getAll().parallelStream()
+            .map(LinhaOnibusMapper::mapToModel)
+            .collect(Collectors.toList());
+        List<LinhaOnibusModel> linhasFromAPI = this.restService.findAll().parallelStream()
+            .filter(linha -> !linhasFromDB.contains(linha))
+            .collect(Collectors.toList());
+
+        linhasFromAPI.addAll(linhasFromDB);
+
+        return linhasFromAPI;
     }
 
     @Override
-    public LinhaOnibusModel add(LinhaOnibusModel linhaOnibusModel) {
-        // TODO: Implementation
-        return null;
+    public LinhaOnibusModel add(LinhaOnibusModel model) {
+        final List<LinhaOnibusModel> linhasFromAPI = this.restService.findAll().parallelStream()
+            .filter(linha -> linha.getCodigo().contentEquals(model.getCodigo()) && linha.getNome().contentEquals(model.getNome()))
+            .collect(Collectors.toList());
+
+        if (!linhasFromAPI.isEmpty()) {
+            final String reason = String.format("LinhaOnibus with properties [codigo='%s', nome='%s'] already exists.", model.getCodigo(), model.getNome());
+            throw new ResponseStatusException(HttpStatus.CONFLICT, reason);
+        }
+
+        final LinhaOnibus linha = LinhaOnibusMapper.mapToDAO(model);
+        this.dao.save(linha);
+
+        return new LinhaOnibusModel(linha);
     }
 
     @Override
-    public LinhaOnibusModel update(long id, LinhaOnibusModel linhaOnibusModel) {
-        // TODO: Implementation
-        return null;
+    public LinhaOnibusModel update(long id, LinhaOnibusModel model) {
+        LinhaOnibus linha = this.dao.get(id);
+
+        if (linha == null) {
+            final LinhaOnibusModel linhaModel = this.restService.findById(id);
+            final String codigo = model.getCodigo() == null ? linhaModel.getCodigo() : model.getCodigo();
+            final String nome = model.getNome() == null ? linhaModel.getNome() : model.getNome();
+            linha = new LinhaOnibus(codigo, nome);
+            linha.setId(linhaModel.getId());
+            this.dao.save(linha);
+        } else {
+            final String[] params = { model.getCodigo(), model.getNome() };
+            this.dao.update(linha, params);
+        }
+
+        return new LinhaOnibusModel(linha);
     }
 
     @Override
-    public LinhaOnibusModel replace(long id, LinhaOnibusModel linhaOnibusModel) {
-        // TODO: Implementation
-        return null;
+    public LinhaOnibusModel replace(long id, LinhaOnibusModel model) {
+        LinhaOnibus linha = this.dao.get(id);
+
+        if (linha == null) {
+            this.restService.findById(id);
+        } else {
+            this.delete(id);
+        }
+
+        return this.add(model);
     }
 
     @Override
     public LinhaOnibusModel delete(long id) {
-        // TODO: Call other DAO class for deleting entries on table ITINERARIO_UT with "DELETE FROM ITINERARIO_UT WHERE ID_LINHA = ?"
-        // TODO: Implementation
-        return null;
+        final LinhaOnibus linha = this.dao.get(id);
+
+        if (linha == null) {
+            final LinhaOnibusModel linhaFromAPI = this.restService.findById(id);
+            if (linhaFromAPI != null) {
+                final String reason = String.format("LinhaOnibus ID %d exists only on external DB, thus you can't delete it.", id);
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, reason);
+            }
+        } else {
+            // TODO: Call other DAO class for deleting entries on table ITINERARIO_UT with "DELETE FROM ITINERARIO_UT WHERE ID_LINHA = ?"
+            this.delete(id);
+        }
+
+        return LinhaOnibusMapper.mapToModel(linha);
     }
 }
