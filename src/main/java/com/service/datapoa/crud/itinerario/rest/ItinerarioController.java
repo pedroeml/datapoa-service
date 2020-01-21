846970f5b24040d3080616de924127c9d93b4932
Pedro Lemos <pedroemlemos@gmail.com>
2020-01-21 17:29:23 -0300

    ItinerarioController CRUD operations

--- src/main/java/com/service/datapoa/crud/itinerario/rest/ItinerarioController.java
+++ src/main/java/com/service/datapoa/crud/itinerario/rest/ItinerarioController.java
@@ -1,13 +1,11 @@
 package com.service.datapoa.crud.itinerario.rest;
 
 import com.service.datapoa.crud.itinerario.dto.ItinerarioDTO;
+import com.service.datapoa.crud.itinerario.integration.ItinerarioRequest;
 import com.service.datapoa.crud.itinerario.mapper.ItinerarioMapper;
 import com.service.datapoa.crud.itinerario.model.ItinerarioModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -21,6 +19,37 @@ public class ItinerarioController {
     @GetMapping(value = "/{id}")
     public ItinerarioDTO findById(@Valid @PathVariable long id) {
         final ItinerarioModel itinerario = this.service.findById(id);
+
+        return ItinerarioMapper.mapToDTO(itinerario);
+    }
+
+    @PostMapping(value = "")
+    public ItinerarioDTO add(@RequestBody ItinerarioRequest request) {
+        ItinerarioModel itinerario = ItinerarioMapper.mapFromRequest(request);
+        itinerario = this.service.add(itinerario);
+
+        return ItinerarioMapper.mapToDTO(itinerario);
+    }
+
+    @PatchMapping(value ="/{id}")
+    public ItinerarioDTO update(@Valid @PathVariable long id, @RequestBody ItinerarioRequest request) {
+        ItinerarioModel itinerario = ItinerarioMapper.mapFromRequest(request);
+        itinerario = this.service.update(id, itinerario);
+
+        return ItinerarioMapper.mapToDTO(itinerario);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ItinerarioDTO replace(@Valid @PathVariable long id, @RequestBody ItinerarioRequest request) {
+        ItinerarioModel itinerario = ItinerarioMapper.mapFromRequest(request);
+        itinerario = this.service.replace(id, itinerario);
+
+        return ItinerarioMapper.mapToDTO(itinerario);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ItinerarioDTO delete(@Valid @PathVariable long id) {
+        final ItinerarioModel itinerario = this.service.delete(id);
 
         return ItinerarioMapper.mapToDTO(itinerario);
     }
